9bdd859b6f4fc197d6123d5b130040bc8d204b24
Divyansh Kaushik <kaushik.divyansh@icloud.com>
2021-01-25 21:18:21 -0600

    updated readme.md

--- README.md
+++ README.md
@@ -1,34 +1,13 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+![Logo](public/logo.png)
+# kaushikdivyansh.dev - v2
+  The second iteration of <a href="https://kaushikdivyansh.dev" target="_blank">kaushikdivyansh.dev</a> built with <a href="https://reactjs.org" target="_blank">React</a> and hosted with <a href="https://www.netlify.com/" target="_blank">Netlify.</a>
 
-## Getting Started
+  Previous iteration:
+  <a href="https://github.com/kaushikdivyansh/Personal-Website-v1" target="_blank">v1</a>,
 
-First, run the development server:
+  [![Netlify Status](https://api.netlify.com/api/v1/badges/614eee5d-9318-46f4-9d56-c96aba3f62b0/deploy-status)](https://app.netlify.com/sites/dk-personal-website-v2/deploys)
 
-```bash
-npm run dev
-# or
-yarn dev
-```
+![Demo](public/demo.png)
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+<hr />
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
-
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
-
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/import?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,65 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <meta charset="utf-8" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+
+        <link rel="manifest" href="/manifest.json" />
+
+        <title>Divyansh Kaushik | Front-End Developer</title>
+        <meta
+          name="description"
+          content="Divyansh Kaushik is a Front-End Developer based in Austin, TX who specializes in building websites and applications."
+        />
+
+        {/* <!-- Google / Search Engine Tags --> */}
+        <meta
+          itemprop="name"
+          content="Divyansh Kaushik | Front-End Developer"
+        />
+        <meta
+          itemprop="description"
+          content="Divyansh Kaushik is a Front-End Developer based in Austin, TX who specializes in building websites and applications."
+        />
+        <meta itemprop="image" content="/demo.png" />
+
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:url" content="https://www.kaushikdivyansh.dev" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Divyansh Kaushik | Front-End Developer"
+        />
+        <meta
+          property="og:description"
+          content="Divyansh Kaushik is a Front-End Developer based in Austin, TX who specializes in building websites and applications."
+        />
+        <meta property="og:image" content="/demo.png" />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="Divyansh Kaushik | Front-End Developer"
+        />
+        <meta
+          name="twitter:description"
+          content="Divyansh Kaushik is a Front-End Developer based in Austin, TX who specializes in building websites and applications."
+        />
+        <meta name="twitter:image" content="/demo.png" />
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-138577500-3"
+        ></script>
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag(){" "}
+          {dataLayer.push(arguments)}
+          gtag("js", new Date()); gtag("config", "UA-138577500-3");
+        </script>
       </Head>
 
       <Layout>
